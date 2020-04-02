bee990d0bfbbd52f9fc2253f0a7a1fd024f16a3f
Tony Tallierchio <ttallierchio@wayfair.com>
2020-04-02 10:04:08 -0400

    make gcp work

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-from fastapi import FastAPI
 from joke import Joke
 import os
-app = FastAPI()
 
-
-@app.get('/api/joke')
-def get(request):
+def joke(request):
     j = Joke()
     return j.GetJokePair()
-@app.get('/api/os_environ')
-def get():
-    l = os.environ
-    return l
